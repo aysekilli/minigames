8c113c7f1af456cd2374451f2ef3dd4b51abe470
Ayşe <aysekilli@posta.mu.edu.tr>
2025-05-21 20:32:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Mini madlib game
+
+color = input("Please enter a color: ")
+name = input("Please enter a plural name: ")
+celeb = input("Please enter a celebrity: ")
+
+print("Roses are",color + ",")
+print(name.capitalize(),"are blue.")
+print("I love", celeb.title() +".")
